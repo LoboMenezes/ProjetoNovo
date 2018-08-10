f1c8d2f1354fd07ba9b5177b4acc8f5fef8f5673
jeff56 <42048091+jeff56@users.noreply.github.com>
2018-08-10 19:37:28 -0300

    sobre atualizada

---
+++ sobre.php
@@ -0,0 +1,17 @@
+﻿<?php
+$titulo = "Home Page"; // Título desta página
+$css = "css/sobre.css"; // CSS desta página
+$botao = "sobre"; // Botão que ficará ativo no menu principal
+require ('_header.php'); // Executa o cabeçalho da página
+?>
+
+
+<p> Seja Bem-Vindo ao site que criamos sobre "Lobos" utilizamos HTML ,CSS3 e PHP é nosso primeira site que estamos criando.</p>
+<p> Colocamos Banco de Dados para utilização de contatos ,mensagem e email e também botamos imagens e noticias sobre lobos .</p>
+<p> Salvamos e separamos alguns arquivos no formato PHP :footer,header,artigos,comn,contatos,index,leartigo,noticias e sobre.</P>
+
+<img src="img/wol.jpg">
+
+<?php
+require ('_footer.php');
+?>
