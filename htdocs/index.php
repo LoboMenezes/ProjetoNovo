c76e56cb04d670ee3a6f3137cf3231d4ea33a02c
rapfjb <41973773+rapfjb@users.noreply.github.com>
2018-08-14 19:30:43 -0300

    Add files via upload

--- htdocs/index.php
+++ htdocs/index.php
@@ -59,8 +59,8 @@ require ('_header.php'); // Executa o cabeçalho da página
 
 <!-- Second Container -->
 <div class="container-fluid bg-2 text-center">
-  <h3 class="margin">Tudo sobre o mundo animal</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+  <h3 class="margin">Tudo sobre o mundo dos lobos.</h3>
+  <p>Os lobos são mamíferos carnívoros, pertencentes à família canidae, a mesma de cães, raposas, etc. É parente bem próximo do cão doméstico (Canis lupus familiaris) e acredita-se que este tenha se originado através da domesticação de lobos cinzentos.</p>
   <a href="#" class="btn btn-default btn-lg">
     <span class="glyphicon glyphicon-search"></span> Search
   </a>
@@ -71,15 +71,15 @@ require ('_header.php'); // Executa o cabeçalho da página
   <h3 class="margin">Where To Find Me?</h3><br>
   <div class="row">
     <div class="col-sm-4">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Um estudo realizado pela Universidade de Stanford revelou que os lobos negros contam com essa coloração devido a uma mutação que ocorre apenas em cães domésticos. Sendo assim, eles são o resultado da cruza de cachorros com lobos no passado.</p>
       <img src="img/lobo1.png" class="img-responsive margin" style="width:50%" alt="Image">
     </div>
     <div class="col-sm-4"> 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Os lobos são animais carnívoros e, em muitos casos, vivem em locais nos quais não existe muita abundância de caça. Assim, extremamente oportunistas, eles nunca desperdiçam uma boa oportunidade de forrar os estômagos.</p>
       <img src="img/lobo4.png" class="img-responsive margin" style="width:50%" alt="Image">
     </div>
     <div class="col-sm-4"> 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Apesar de não faltarem casos de pessoas que conviveram — ou convivem — com lobos, a verdade é quase impossível que esses animais sejam domesticados. Quando filhotes, seu comportamento é bem parecido ao de cães domésticos.</p>
       <img src="img/lobo3.png" class="img-responsive margin" style="width:50%" alt="Image">
     </div>
   </div>
